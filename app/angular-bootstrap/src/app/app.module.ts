c3092dad78fe7e0d6deb4fd7529b8429cb6416a2
Tavo Soto <gsoto@itx.com>
2022-08-10 17:51:15 -0500

    Use ng generate cli script to create new servers component and nest previous server components inside

--- app/angular-bootstrap/src/app/app.module.ts
+++ app/angular-bootstrap/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   // NOTE: Register New Component Modules here!
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
