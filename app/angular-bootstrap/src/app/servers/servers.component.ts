593d61acb75dbf23cf7aeadc8e0c363ebaa61ba4
Tavo Soto <gsoto@itx.com>
2022-08-10 18:19:15 -0500

    Examples plus docs for different types of supported angular selectors

--- app/angular-bootstrap/src/app/servers/servers.component.ts
+++ app/angular-bootstrap/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',   // NOTE: Select html element by tag (Most common for components!)
+  // selector: '[app-servers]', // NOTE: Select html by attribute
+  selector: '.app-servers',     // NOTE: Select html by css class
   template: `
             <app-server></app-server>
             <app-server></app-server>
