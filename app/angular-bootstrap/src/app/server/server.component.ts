c3b0cd7cfb9928c6adb238326a65ccde6054c0e2
Tavo Soto <gsoto@itx.com>
2022-08-16 10:16:11 -0500

    Example of string interpolation using component var and function

--- app/angular-bootstrap/src/app/server/server.component.ts
+++ app/angular-bootstrap/src/app/server/server.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  serverID: number = 7;
+  serverStatus: string = 'Offline';
 
+  /**
+   * Get Server Status
+   *
+   * This is an example of how to write a function in
+   * typescript. This specific method will return the pre-set
+   * class variable serverStatus defined above.
+   *
+   * @returns {string} The pre-set serverStatus value in this class.
+   */
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
