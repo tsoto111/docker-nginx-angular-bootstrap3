a1564651dca8fd0f137dea7d06ec49055a2aa7dc
Tavo Soto <gsoto@itx.com>
2022-08-17 19:34:14 -0500

    Final project solution of putting everything together thus far using change password view

--- app/angular-bootstrap/src/app/app.module.ts
+++ app/angular-bootstrap/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { PasswordInputComponent } from './forms/password-input/password-input.component';
 
 @NgModule({
   // NOTE: Register New Component Modules here!
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    PasswordInputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/angular-bootstrap/src/app/forms/password-input/password-input.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-input',
+  templateUrl: './password-input.component.html',
+  styleUrls: ['./password-input.component.css']
+})
+export class PasswordInputComponent implements OnInit {
+  @Input() label: string = ''
+
+  showPassword: boolean = false;
+  showHideCount: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  /**
+   * Get Password Input Type
+   *
+   * This function will toggle the password form input type
+   * depnding on the current state of the showPassword boolean.
+   *
+   * @returns {string}
+   */
+  getPasswordInputType = (): string => {
+    return this.showPassword === true ? 'text' : 'password';
+  }
+
+  /**
+   * Toggle Password Visibility
+   *
+   * Toggles the class variable of showPassword on button click
+   * to show/hide password input text.
+   *
+   * @returns {void}
+   */
+  togglePasswordVisibility = (): void => {
+    this.showHideCount += 1;
+    this.showPassword = !this.showPassword
+  }
+}
